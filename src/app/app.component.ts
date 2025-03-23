7d91fd9cca4035ecf9a7beeeffe8297ebff2ecf8
Ignacy <kazmiig_aehit@students.vizja.pl>
2025-03-23 11:21:18 +0100

    :))

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {SwapiConnectorService} from './services/swapi-connector.service';
+import {SwapiOPerson} from './domain/types';
 
 @Component({
   selector: 'app-root',
@@ -8,4 +10,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'soft-engineering-project-l3';
+
+  protected results: SwapiOPerson[] = [];
+
+  constructor(private swapi: SwapiConnectorService) {
+    this.swapi.getData().subscribe(response => {
+      this.results = response.results.map((object: any) => {
+        return {
+          name: object.name,
+          height: parseInt(object.height),
+          birth_year: object.birth_year,
+          films: object.films,
+        } as SwapiOPerson
+      })
+      console.log(response.results);
+    });
+  }
 }
